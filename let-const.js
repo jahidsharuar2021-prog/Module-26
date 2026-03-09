aca73ed7c878fdbe814e57f96ab3698d77d05d5c
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-03-09 10:22:17 +0600

    summestion function

--- let-const.js
+++ let-const.js
@@ -1,3 +1,12 @@
 const name='Bangladesh';
-const name='Bangladesh';
 console.log(name);
+
+
+function summestion(num1 ,num2){
+    const sum=num1+num2;
+    
+    console.log(num1,num2,sum);
+}
+
+summestion(10);
+

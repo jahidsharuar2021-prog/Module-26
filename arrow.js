e318e84dc3e3be9c6c111c1679b33fe8a90a0136
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-03-09 11:42:23 +0600

    arrow function

--- arrow.js
+++ arrow.js
@@ -1,4 +1,4 @@
-console.log(sum(10,20));
+// console.log(sum(10,20));
 
  function sum(Number1,Number2){
     return Number1+Number2;
@@ -8,4 +8,12 @@ console.log(sum(10,20));
  const summestion=function(Number3,Number4){
     return Number3+Number4;
 };
-console.log(summestion(10,50));
+// console.log(summestion(10,50));
+
+// arrow function
+const multiply=(a,b)=> a*b;
+console.log(multiply(10,20));
+
+// addition function
+const add=(a,b)=> a+b;
+console.log(add(10,20));
